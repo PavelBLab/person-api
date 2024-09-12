4bb79ad9f686af4de89f4a82d9dd53652ae9da99
pavelbrustinov <brustinov.pavel@gmail.com>
2024-09-12 18:40:42 +0200

    Added Person.java

---
+++ src/main/java/com/testcase/api/persons/persistence/entities/Person.java
@@ -0,0 +1,54 @@
+package com.testcase.api.persons.persistence.entities;
+
+import com.testcase.api.persons.provider.models.PersonGender;
+import com.testcase.api.persons.provider.models.PersonStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    private UUID id;
+
+    private String name;
+    private String surname;
+    private LocalDate dateOfBirth;
+    private String country;
+    private String jobTitle;
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private PersonStatus status;
+
+    private String employer;
+
+    @Enumerated(EnumType.STRING)
+    private PersonGender gender;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) && Objects.equals(dateOfBirth, person.dateOfBirth);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, dateOfBirth);
+    }
+
+}
