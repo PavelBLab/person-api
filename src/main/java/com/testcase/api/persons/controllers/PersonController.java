7742e0f6328a711b249f5bc6b8b6a3bc970a0e5c
pavelbrustinov <brustinov.pavel@gmail.com>
2024-09-12 19:20:32 +0200

    Added PersonController.java

---
+++ src/main/java/com/testcase/api/persons/controllers/PersonController.java
@@ -0,0 +1,46 @@
+package com.testcase.api.persons.controllers;
+
+import com.testcase.api.persons.converters.PersonConverter;
+import com.testcase.api.persons.provider.controllers.PersonApi;
+import com.testcase.api.persons.provider.models.PersonDto;
+import com.testcase.api.persons.provider.models.PersonMiniDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequiredArgsConstructor
+public class PersonController implements PersonApi {
+
+    private final PersonConverter personConverter;
+
+    @Override
+    public ResponseEntity<List<PersonDto>> allPersons(final String personName, final String personSurname) {
+        return ResponseEntity.ok(personConverter.getAll(personName, personSurname));
+    }
+
+    @Override
+    public ResponseEntity<PersonDto> createPerson(final PersonDto personIncomingDto) {
+        return new ResponseEntity<>(personConverter.createOne(personIncomingDto), CREATED);
+    }
+
+    @Override
+    public ResponseEntity<PersonDto> onePerson(final UUID personId) {
+        return ResponseEntity.ok(personConverter.getOne(personId));
+    }
+
+    @Override
+    public ResponseEntity<PersonDto> updatePerson(final UUID personId, final PersonMiniDto personMiniDto) {
+        return personId == null || personMiniDto == null ?
+                new ResponseEntity<>(HttpStatus.BAD_REQUEST) :
+                new ResponseEntity<>(personConverter.updateOne(personId, personMiniDto), OK);
+    }
+
+}
